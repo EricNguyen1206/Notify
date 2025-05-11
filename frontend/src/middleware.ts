862e5b660bbf0c76b17a9a889944d23828c4bb8b
tinnt1 <tinnt1@runsystem.net>
2025-05-11 23:36:05 +0700

    fix: FE fix config Auth and login, register flow

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,7 +1,13 @@
 import NextAuth from "next-auth";
 import { authConfig } from "./lib/auth.config";
+import { NextRequest } from "next/server";
 
-export default NextAuth(authConfig).auth;
+// export default NextAuth(authConfig).auth;
+
+export default function middleware(req: NextRequest) {
+  const isLoggedIn = !!req.cookies.get("next-auth.session-token");
+  console.log("TEST", isLoggedIn);
+}
 
 export const config = {
   matcher: ["/((?!api|static|.*\\..*|_next).*)"],
